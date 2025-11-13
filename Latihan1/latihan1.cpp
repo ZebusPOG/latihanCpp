59a84de152434f780a2f2b5fe6221e8a9f2f71ff
Justin Jeremy Asta <justinasta14@MacBook-Pro-Justin.local>
2025-11-13 08:01:57 +0700

    Justin

---
+++ Latihan1/latihan1.cpp
@@ -0,0 +1,23 @@
+//Program Bilangan Palindrom
+#include <iostream>
+using namespace std;
+int main(){
+    int bilanganAwal;
+    cin >> bilanganAwal;
+    int temp = bilanganAwal * 1;
+    int bilanganBaru = 0;
+    int sisa;
+    do{
+        sisa = temp % 10;
+        bilanganBaru = bilanganBaru * 10 + sisa;
+        temp /= 10;
+    }
+    while(temp > 0);
+    if(bilanganAwal == bilanganBaru){
+        cout << "Bilangan Palindrom";
+    }
+    else{
+        cout << "Bukan Palindrom";
+    }
+    return 0;
+}
